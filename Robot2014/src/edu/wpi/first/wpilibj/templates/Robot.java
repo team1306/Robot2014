bf802e161622de944806ab524b297482f85cbcf4
Finn Voichick <fvoichick@gmail.com>
2014-01-19 13:40:08 -0600

    Uses XBOX controls now

--- Robot2014/src/edu/wpi/first/wpilibj/templates/Robot.java
+++ Robot2014/src/edu/wpi/first/wpilibj/templates/Robot.java
@@ -4,16 +4,14 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
-
 
 import edu.wpi.first.wpilibj.Jaguar;
 import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SimpleRobot;
 import org.badgerbots.lib.TankDrive;
-import org.badgerbots.lib.TankDriveJoy;
-
+import org.badgerbots.lib.TankDriveXbox;
+import org.badgerbots.lib.XBoxController;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -23,41 +21,47 @@ import org.badgerbots.lib.TankDriveJoy;
  * directory.
  */
 public class Robot extends SimpleRobot {
+
     private final Jaguar leftMotor;
     private final Jaguar rightMotor;
     private final Joystick leftJoy;
     private final Joystick rightJoy;
+    private final XBoxController xbox;
     private final TankDrive drive;
-    
+
     public Robot() {
         leftMotor = new Jaguar(1);
         rightMotor = new Jaguar(2);
+        
         leftJoy = new Joystick(1);
         rightJoy = new Joystick(2);
         
-        drive = new TankDriveJoy(leftMotor, rightMotor, 2, false, 0.1, 1.0, 0.2, leftJoy, rightJoy);
+        xbox = new XBoxController(3);
+        
+        drive = new TankDriveXbox(leftMotor, rightMotor, 2.0, false, 0.1, 1.0, 0.2, xbox);
     }
+
     /**
      * This function is called once each time the robot enters autonomous mode.
      */
     public void autonomous() {
-        
+
     }
 
     /**
      * This function is called once each time the robot enters operator control.
      */
     public void operatorControl() {
-        while(isOperatorControl()) {
+        while (isOperatorControl()) {
             drive.drive();
         }
     }
-    
+
     /**
      * This function is called once each time the robot enters test mode.
      */
     public void test() {
-    
+
     }
 }
 //test from finn
